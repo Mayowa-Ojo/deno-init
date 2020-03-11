3e54cfedc03e40aa6f37fa502533a834e4c7b736
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-03-11 02:14:25 -0600

    feat: create module to handle dependencies

---
+++ deps.ts
@@ -0,0 +1,10 @@
+import { parse } from "flags/mod.ts";
+import { readLines } from "io/bufio.ts";
+import { exists, writeFileStr } from "fs/mod.ts";
+
+export {
+   parse,
+   readLines,
+   exists,
+   writeFileStr
+}
