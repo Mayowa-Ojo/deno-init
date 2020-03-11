7086968475da4d0c9c45212e2348b82e46a01121
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-03-11 02:52:37 -0600

    feat: add fmt dependency

--- deps.ts
+++ deps.ts
@@ -1,10 +1,12 @@
 import { parse } from "flags/mod.ts";
 import { readLines } from "io/bufio.ts";
 import { exists, writeFileStr } from "fs/mod.ts";
+import * as c from "fmt/mod.ts";
 
 export {
    parse,
    readLines,
    exists,
-   writeFileStr
+   writeFileStr,
+   c
 }
