3daaa89b5b955048b64594affba2a0bb4b09e2b1
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-03-13 11:38:57 -0600

    feat: add file content

--- utils/file_content.ts
+++ utils/file_content.ts
@@ -4,8 +4,21 @@ const content_makefile: string = `
 init: 
    deno mod.ts
 
+`;
+
+const content_importmap:string = `
+{
+   "imports": {
+      "flags/": "https://deno.land/std@v0.35.0/flags/",
+      "fs/": "https://deno.land/std@v0.35.0/fs/",
+      "io/": "https://deno.land/std@v0.35.0/io/",
+      "log/": "https://deno.land/std@v0.35.0/log/",
+      "fmt/": "https://deno.land/std@v0.35.0/fmt/"
+   }
+}
 `
 
 export {
-   content_makefile
+   content_makefile,
+   content_importmap
 }
