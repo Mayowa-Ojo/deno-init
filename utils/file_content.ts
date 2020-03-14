f9ac15d41840c5136a600ad6d2bdd9af7a5287c5
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-03-13 18:26:12 -0600

    feat: add tsconfig content'

--- utils/file_content.ts
+++ utils/file_content.ts
@@ -1,4 +1,4 @@
-const content_makefile: string = `
+export const content_makefile: string = `
 # make commands
 
 init: 
@@ -6,7 +6,7 @@ init:
 
 `;
 
-const content_importmap:string = `
+export const content_importmap:string = `
 {
    "imports": {
       "flags/": "https://deno.land/std@v0.35.0/flags/",
@@ -16,9 +16,31 @@ const content_importmap:string = `
       "fmt/": "https://deno.land/std@v0.35.0/fmt/"
    }
 }
-`
+`;
 
-export {
-   content_makefile,
-   content_importmap
-}
+export const content_tsconfig = `
+{
+   "compilerOptions": {
+     "allowJs": true,
+     "esModuleInterop": true,
+     "lib": [
+        "esnext",
+        "dom"
+      ],
+     "module": "esnext",
+     "moduleResolution": "node",
+     "emitDecoratorMetadata": true,
+     "experimentalDecorators": true,
+     "noEmit": true,
+     "pretty": true,
+     "resolveJsonModule": true,
+     "target": "esnext",
+     "baseUrl": "src",
+      "paths": {
+      }
+   },
+   "include": [
+     "./**/*.ts"
+   ]   
+ }
+`;
