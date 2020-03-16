3b6bffd406662bd37350be11efb686b89702e792
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-03-15 23:20:57 -0600

    refactor: move utility functions into modules

--- deps.ts
+++ deps.ts
@@ -2,6 +2,7 @@ import { parse } from "flags/mod.ts";
 import { readLines } from "io/bufio.ts";
 import { exists, writeFileStr, ensureDir } from "fs/mod.ts";
 import * as c from "fmt/mod.ts";
+import { assertEquals } from "testing/asserts.ts";
 
 export {
    parse,
@@ -9,5 +10,6 @@ export {
    exists,
    writeFileStr,
    ensureDir,
+   assertEquals,
    c
 }
